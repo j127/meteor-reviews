c757db224b35a9bde01fbbfee338668ab464b9cb
J. Cohen <hostelmarketing@gmail.com>
2015-11-26 12:35:42 -0800

    Add styling, more star images

--- client/templates/layout.js
+++ client/templates/layout.js
@@ -3,3 +3,20 @@ Template.registerHelper('truncateText', function (text, len) {
     newText = newText.substr(0, Math.min(newText.length, newText.lastIndexOf(' ')));
     return new Spacebars.SafeString(newText);
 });
+
+Template.registerHelper('getAverage', function (reviews) {
+    var sum = 0;
+    for (var i = 0, iLen = reviews.length; i < iLen; i++) {
+        sum += parseInt(reviews[i].rating, 10);
+    }
+    var avg = sum / reviews.length;
+    return Math.round(avg);
+});
+
+Template.registerHelper('getReviewsTotal', function (total) {
+    if (total > 0) {
+        return total;
+    } else {
+        return 0;
+    }
+});
