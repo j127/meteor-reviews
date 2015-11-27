213cf877139bc4dc4b2f6e138283882db3d58af2
J. Cohen <hostelmarketing@gmail.com>
2015-11-27 14:49:49 -0800

    Remove insecure, move method to methods

--- client/templates/addProduct/addProduct.js
+++ client/templates/addProduct/addProduct.js
@@ -7,32 +7,9 @@ Template.addProduct.events({
         var isFeatured = e.target.isFeatured.value;
 
         var file = $('#productImage').get(0).files[0];
-        if (file) {
-            fsFile = new FS.File(file);
-            ProductsImages.insert(fsFile, function (err, result) {
-                if (!err) {
-                    var productImage = '/cfs/files/ProductsImages/' + result._id;
-                    Products.insert({
-                        name: name,
-                        category: category,
-                        description: description,
-                        isFeatured: isFeatured,
-                        image: productImage,
-                        createdAt: new Date()
-                    });
-                }
-            });
-        } else {
-            var productImage = '/img/noimage.png';
-            Products.insert({
-                name: name,
-                category: category,
-                description: description,
-                isFeatured: isFeatured,
-                image: productImage,
-                createdAt: new Date()
-            });
-        }
+
+        Meteor.call('addProduct', file, name, category, description, isFeatured);
+
         e.target.name.value = '';
         e.target.category.value = '';
         e.target.description.value = '';
